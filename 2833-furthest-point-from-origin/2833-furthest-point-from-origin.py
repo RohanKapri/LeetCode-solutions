36e4d00c0fab58c8bd17a0acea0e789aef436601
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-04-24 17:17:41 +0530

    Time: 0 ms (100%), Space: 17.6 MB (100%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.py
@@ -0,0 +1,6 @@
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        left = moves.count('L')
+        right = moves.count('R')
+        underscore = moves.count('_')
+        return abs(left - right) + underscore
