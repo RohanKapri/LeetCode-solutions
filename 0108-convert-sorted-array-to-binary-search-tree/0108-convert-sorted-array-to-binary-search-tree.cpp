d48d0322b2d9c4875c26831896a8daff18f1b4cd
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-04-28 09:14:00 +0530

    Time: 2 ms (71.14%), Space: 22.8 MB (80.5%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -1,19 +1,26 @@
-// For Junko F. Didi and Shree DR.MDD
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* quantumGravitationalSubdivision(vector<int>& cosmologicalSequenceArray, int spacetimeLeftBoundaryIndex, int spacetimeRightBoundaryIndex){
-        if(spacetimeLeftBoundaryIndex > spacetimeRightBoundaryIndex){
-            return nullptr;
-        }
-        int quantumMidpointSingularityIndex = spacetimeLeftBoundaryIndex + (spacetimeRightBoundaryIndex - spacetimeLeftBoundaryIndex) / 2;
-        TreeNode* universalRootNode = new TreeNode(cosmologicalSequenceArray[quantumMidpointSingularityIndex]);
-        universalRootNode->left = quantumGravitationalSubdivision(cosmologicalSequenceArray, spacetimeLeftBoundaryIndex, quantumMidpointSingularityIndex - 1);
-        universalRootNode->right = quantumGravitationalSubdivision(cosmologicalSequenceArray, quantumMidpointSingularityIndex + 1, spacetimeRightBoundaryIndex);
-        return universalRootNode;
-    }
-
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return quantumGravitationalSubdivision(nums, 0, nums.size() - 1);
+        return buildTree(nums, 0, nums.size() - 1);
+    }
+    
+    TreeNode* buildTree(vector<int>& nums, int left, int right) {
+        if (left > right) return nullptr;
+        int mid = left + (right - left) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = buildTree(nums, left, mid - 1);
+        root->right = buildTree(nums, mid + 1, right);
+        return root;
     }
 };
