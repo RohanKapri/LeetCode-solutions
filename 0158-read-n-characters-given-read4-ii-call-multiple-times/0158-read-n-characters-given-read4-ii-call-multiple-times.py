a9d210d930969403c2c3b621e29250ad8306d0a1
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-04-28 10:45:50 +0530

    Time: 49 ms (27.37%), Space: 19.3 MB (48.77%) - LeetHub

--- 0158-read-n-characters-given-read4-ii-call-multiple-times/0158-read-n-characters-given-read4-ii-call-multiple-times.py
+++ 0158-read-n-characters-given-read4-ii-call-multiple-times/0158-read-n-characters-given-read4-ii-call-multiple-times.py
@@ -1,28 +1,23 @@
 # For Junko F. Didi and Shree DR.MDD
 
-class Solution(object):
+class Solution:
     def __init__(self):
-        self.quantumResidualPointer = 0
-        self.quantumBufferLimit = 0
-        self.quantumFourDimensionalBuffer = ["" for _ in range(4)]
+        self.quantumTemporalBufferArray = [''] * 4
+        self.quantumTraversalPointerIndex = 0
+        self.quantumAvailableSpectrumSize = 0
 
-    def read(self, buf, n):
-        if n == 0:
-            return 0
+    def read(self, buf: List[str], n: int) -> int:
+        quantumExtractedCharacterFlux = 0
 
-        extractedQuantumUnits = 0
-
-        while extractedQuantumUnits < n:
-
-            if self.quantumResidualPointer < self.quantumBufferLimit:
-                buf[extractedQuantumUnits] = self.quantumFourDimensionalBuffer[self.quantumResidualPointer % 4]
-                self.quantumResidualPointer += 1
-                extractedQuantumUnits += 1
-            else:
-                newlyCollapsedWaveCount = read4(self.quantumFourDimensionalBuffer)
-                if newlyCollapsedWaveCount == 0:
+        while quantumExtractedCharacterFlux < n:
+            if self.quantumTraversalPointerIndex == self.quantumAvailableSpectrumSize:
+                self.quantumAvailableSpectrumSize = read4(self.quantumTemporalBufferArray)
+                self.quantumTraversalPointerIndex = 0
+                if self.quantumAvailableSpectrumSize == 0:
                     break
-                self.quantumResidualPointer = 0
-                self.quantumBufferLimit = newlyCollapsedWaveCount
 
-        return extractedQuantumUnits
+            buf[quantumExtractedCharacterFlux] = self.quantumTemporalBufferArray[self.quantumTraversalPointerIndex]
+            quantumExtractedCharacterFlux += 1
+            self.quantumTraversalPointerIndex += 1
+
+        return quantumExtractedCharacterFlux
