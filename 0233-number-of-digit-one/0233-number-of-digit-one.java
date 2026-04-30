1b6ffef4b6f405357da95440b814b46ae5b8e26b
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-04-30 09:26:53 +0530

    Time: 0 ms (100%), Space: 41.9 MB (85.22%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int countDigitOne(int n) {
+        long count = 0;
+        long factor = 1;
+        while (factor <= n) {
+            long lowerNumbers = n - (n / factor) * factor;
+            long currentDigit = (n / factor) % 10;
+            long higherNumbers = n / (factor * 10);
+            
+            if (currentDigit == 0) {
+                count += higherNumbers * factor;
+            } else if (currentDigit == 1) {
+                count += higherNumbers * factor + lowerNumbers + 1;
+            } else {
+                count += (higherNumbers + 1) * factor;
+            }
+            
+            factor *= 10;
+        }
+        return (int) count;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countDigitOne(13));
+        System.out.println(solution.countDigitOne(0));
+    }
+}
