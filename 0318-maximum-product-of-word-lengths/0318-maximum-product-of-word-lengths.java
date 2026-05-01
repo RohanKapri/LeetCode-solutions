76ed9a8d10389be8407d9e2da7e3a52006da24c7
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-01 10:11:31 +0530

    Time: 9 ms (99.14%), Space: 47.4 MB (99.81%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+    public int maxProduct(String[] words) {
+        int n = words.length;
+        int[] bitmasks = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (char c : words[i].toCharArray()) {
+                bitmasks[i] |= 1 << (c - 'a');
+            }
+        }
+        
+        int maxProduct = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((bitmasks[i] & bitmasks[j]) == 0) {
+                    maxProduct = Math.max(maxProduct, words[i].length() * words[j].length());
+                }
+            }
+        }
+        
+        return maxProduct;
+    }
+}
