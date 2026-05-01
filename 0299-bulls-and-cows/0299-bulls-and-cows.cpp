d1801bf730457f46a4e5d70033235566638b851d
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-01 09:57:54 +0530

    Time: 0 ms (100%), Space: 8.9 MB (54.67%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls = 0, cows = 0;
+        vector<int> secretCount(10, 0), guessCount(10, 0);
+
+        // First pass: count bulls and record frequencies of non-bull digits
+        for (int i = 0; i < secret.size(); i++) {
+            if (secret[i] == guess[i]) {
+                bulls++;
+            } else {
+                secretCount[secret[i] - '0']++;
+                guessCount[guess[i] - '0']++;
+            }
+        }
+
+        // Second pass: count cows based on minimum of unmatched digits
+        for (int i = 0; i < 10; i++) {
+            cows += min(secretCount[i], guessCount[i]);
+        }
+
+        return to_string(bulls) + "A" + to_string(cows) + "B";
+    }
+};
