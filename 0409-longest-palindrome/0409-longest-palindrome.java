5d6388a32ce0107e91a1102d98957d7e9127027b
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-02 09:45:47 +0530

    Time: 1 ms (100%), Space: 41.6 MB (99.97%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for (char c : s.toCharArray()) count[c]++;
+        int result = 0;
+        for (int i : count) {
+            result += i / 2 * 2;
+            if (i % 2 == 1 && result % 2 == 0) result++;
+        }
+        return result;
+    }
+}
