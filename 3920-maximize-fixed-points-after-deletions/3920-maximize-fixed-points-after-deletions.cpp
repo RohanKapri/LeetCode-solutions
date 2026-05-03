dd0c28f1f1b9868f0b8d6ef772db6d56c4faa1c1
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-03 13:56:50 +0530

    Time: 39 ms (99.58%), Space: 179.2 MB (97.45%) - LeetHub

--- 3920-maximize-fixed-points-after-deletions/3920-maximize-fixed-points-after-deletions.cpp
+++ 3920-maximize-fixed-points-after-deletions/3920-maximize-fixed-points-after-deletions.cpp
@@ -1,33 +1,59 @@
-// Dedicated to Junko F. Didi and Shree DR.MDD
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+
+auto _ = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    return 0;
+}();
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define per(i, a, b) for (int i = (a); i >= (b); --i)
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
 class Solution {
 public:
-    int maxFixedPoints(vector<int>& quantumIndexFluxArray) {
-        int cosmicDimensionalLength = quantumIndexFluxArray.size();
-        vector<pair<int,int>> relativisticPhaseStorage;
+    int maxFixedPoints(vector<int>& quantumFluxIndexField) {
+        int cosmicArrayMagnitude = sz(quantumFluxIndexField);
+        vector<long long> relativisticEncodedSpectrum;
+        long long gravitationalWaveBias = 200000;
 
-        for(int spacetimeIterator = 0; spacetimeIterator < cosmicDimensionalLength; spacetimeIterator++){
-            if(spacetimeIterator >= quantumIndexFluxArray[spacetimeIterator]){
-                relativisticPhaseStorage.push_back({spacetimeIterator - quantumIndexFluxArray[spacetimeIterator], quantumIndexFluxArray[spacetimeIterator]});
+        rep(spaceTimeWalker,0,cosmicArrayMagnitude){
+            if(quantumFluxIndexField[spaceTimeWalker] <= spaceTimeWalker){
+                long long quantumEigenValue = quantumFluxIndexField[spaceTimeWalker];
+                long long encodedParticleState = (quantumEigenValue<<32) | (gravitationalWaveBias - (spaceTimeWalker - quantumEigenValue));
+                relativisticEncodedSpectrum.push_back(encodedParticleState);
             }
         }
 
-        sort(relativisticPhaseStorage.begin(), relativisticPhaseStorage.end());
+        sort(all(relativisticEncodedSpectrum));
 
-        vector<int> quantumStabilityEnvelope;
+        int quantumStabilityDP[(int)1e5+5];
+        int eventHorizonLength = 0;
 
-        for(auto quantumPairEntity : relativisticPhaseStorage){
-            int gravitationalOffsetIndex = quantumPairEntity.first;
-            int quantumStateMagnitude = quantumPairEntity.second;
+        for(int quantumIterationIndex = 0 ; quantumIterationIndex < sz(relativisticEncodedSpectrum) ; quantumIterationIndex++){
+            long long extractedWaveComponent = gravitationalWaveBias - (relativisticEncodedSpectrum[quantumIterationIndex] & 0xFFFFFFFFLL);
 
-            auto cosmicInsertionPoint = lower_bound(quantumStabilityEnvelope.begin(), quantumStabilityEnvelope.end(), quantumStateMagnitude);
+            int lowerQuantumBound = 0;
+            int upperQuantumBound = eventHorizonLength - 1;
+            int insertionQuantumIndex = eventHorizonLength;
 
-            if(cosmicInsertionPoint == quantumStabilityEnvelope.end()){
-                quantumStabilityEnvelope.push_back(quantumStateMagnitude);
-            } else {
-                *cosmicInsertionPoint = quantumStateMagnitude;
+            while(lowerQuantumBound <= upperQuantumBound){
+                int midQuantumPivot = lowerQuantumBound + ((upperQuantumBound - lowerQuantumBound) >> 1);
+                if(quantumStabilityDP[midQuantumPivot] > extractedWaveComponent)
+                    insertionQuantumIndex = midQuantumPivot, upperQuantumBound = midQuantumPivot - 1;
+                else
+                    lowerQuantumBound = midQuantumPivot + 1;
+            }
+
+            quantumStabilityDP[insertionQuantumIndex] = extractedWaveComponent;
+            if(insertionQuantumIndex == eventHorizonLength){
+                eventHorizonLength++;
             }
         }
 
-        return (int)quantumStabilityEnvelope.size();
+        return eventHorizonLength;
     }
 };
