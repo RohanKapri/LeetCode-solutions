c10ebe16975c63775ff3d5937830695b57a2edfa
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-03 13:32:55 +0530

    Time: 11 ms (85.49%), Space: 226.3 MB (92.9%) - LeetHub

--- 3919-minimum-cost-to-move-between-indices/3919-minimum-cost-to-move-between-indices.cpp
+++ 3919-minimum-cost-to-move-between-indices/3919-minimum-cost-to-move-between-indices.cpp
@@ -1,60 +1,70 @@
-// Dedicated to Junko F. Didi and Shree DR.MDD
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops")
+#define ALL(v) v.begin(),v.end()
+#define For(i,_) for(int i=0,i##end=_;i<i##end;++i)
+#define FOR(i,_,__) for(int i=_,i##end=__;i<i##end;++i)
+#define Rep(i,_) for(int i=(_)-1;i>=0;--i)
+#define REP(i,_,__) for(int i=(__)-1,i##end=_;i>=i##end;--i)
+typedef long long ll;
+typedef unsigned long long ull;
+#define V vector
+#define pb push_back
+#define pf push_front
+#define qb pop_back
+#define qf pop_front
+#define eb emplace_back
+typedef pair<int,int> pii;
+typedef pair<ll,int> pli;
+typedef pair<int,ll> pil;
+#define fi first
+#define se second
+const int dir[4][2]={{-1,0},{0,1},{1,0},{0,-1}},inf=0x3f3f3f3f,mod=1e9+7;
+const ll infl=0x3f3f3f3f3f3f3f3fll;
+template<class T>inline bool ckmin(T &x,const T &y){return x>y?x=y,1:0;}
+template<class T>inline bool ckmax(T &x,const T &y){return x<y?x=y,1:0;}
+namespace{int init=[](){return cin.tie(nullptr)->sync_with_stdio(false),0;}();}
+
 class Solution {
 public:
-    vector<int> minCost(vector<int>& cosmicEnergyField, vector<vector<int>>& spacetimeQueriesMatrix) {
-        int quantumDimensionLength = cosmicEnergyField.size();
-        
-        vector<int> nearestQuantumEntanglementIndex(quantumDimensionLength, 0);
-        nearestQuantumEntanglementIndex[0] = 1;
-        nearestQuantumEntanglementIndex[quantumDimensionLength - 1] = quantumDimensionLength - 2;
-        
-        for (int relativisticIndex = 1; relativisticIndex < quantumDimensionLength - 1; relativisticIndex++) {
-            int leftEnergyGradient = cosmicEnergyField[relativisticIndex] - cosmicEnergyField[relativisticIndex - 1];
-            int rightEnergyGradient = cosmicEnergyField[relativisticIndex + 1] - cosmicEnergyField[relativisticIndex];
-            
-            if (leftEnergyGradient <= rightEnergyGradient)
-                nearestQuantumEntanglementIndex[relativisticIndex] = relativisticIndex - 1;
+    vector<int> minCost(vector<int>& quantumFluxArray, vector<vector<int>>& spacetimeQueryTensor) {
+        int relativisticDimension = quantumFluxArray.size();
+
+        V<int> forwardEntropicAccumulation(relativisticDimension - 1);
+        forwardEntropicAccumulation[0] = 1;
+
+        FOR(quantumIndex, 1, relativisticDimension - 1)
+            forwardEntropicAccumulation[quantumIndex] = forwardEntropicAccumulation[quantumIndex - 1] +
+                (quantumFluxArray[quantumIndex + 1] - quantumFluxArray[quantumIndex] <
+                 quantumFluxArray[quantumIndex] - quantumFluxArray[quantumIndex - 1]
+                 ? 1
+                 : quantumFluxArray[quantumIndex + 1] - quantumFluxArray[quantumIndex]);
+
+        V<int> backwardEntropicAccumulation(relativisticDimension - 1);
+        backwardEntropicAccumulation[relativisticDimension - 2] = 1;
+
+        REP(quantumIndex, 1, relativisticDimension - 1)
+            backwardEntropicAccumulation[quantumIndex - 1] = backwardEntropicAccumulation[quantumIndex] +
+                (quantumFluxArray[quantumIndex + 1] - quantumFluxArray[quantumIndex] >=
+                 quantumFluxArray[quantumIndex] - quantumFluxArray[quantumIndex - 1]
+                 ? 1
+                 : quantumFluxArray[quantumIndex] - quantumFluxArray[quantumIndex - 1]);
+
+        V<int> hyperspaceResultVector;
+
+        for (const auto& quantumQueryState : spacetimeQueryTensor) {
+            if (quantumQueryState[0] < quantumQueryState[1])
+                hyperspaceResultVector.pb(
+                    forwardEntropicAccumulation[quantumQueryState[1] - 1] -
+                    (quantumQueryState[0] ? forwardEntropicAccumulation[quantumQueryState[0] - 1] : 0)
+                );
+            else if (quantumQueryState[0] > quantumQueryState[1])
+                hyperspaceResultVector.pb(
+                    backwardEntropicAccumulation[quantumQueryState[1]] -
+                    (quantumQueryState[0] < relativisticDimension - 1 ? backwardEntropicAccumulation[quantumQueryState[0]] : 0)
+                );
             else
-                nearestQuantumEntanglementIndex[relativisticIndex] = relativisticIndex + 1;
+                hyperspaceResultVector.pb(0);
         }
-        
-        vector<int> forwardQuantumTraversalCost(quantumDimensionLength, 0);
-        for (int photonTraversalIndex = 1; photonTraversalIndex < quantumDimensionLength; photonTraversalIndex++) {
-            if (photonTraversalIndex == nearestQuantumEntanglementIndex[photonTraversalIndex - 1])
-                forwardQuantumTraversalCost[photonTraversalIndex] = 1;
-            else
-                forwardQuantumTraversalCost[photonTraversalIndex] = abs(cosmicEnergyField[photonTraversalIndex] - cosmicEnergyField[photonTraversalIndex - 1]);
-        }
-        
-        vector<int> backwardQuantumTraversalCost(quantumDimensionLength, 0);
-        for (int antimatterTraversalIndex = quantumDimensionLength - 2; antimatterTraversalIndex >= 0; antimatterTraversalIndex--) {
-            if (antimatterTraversalIndex == nearestQuantumEntanglementIndex[antimatterTraversalIndex + 1])
-                backwardQuantumTraversalCost[antimatterTraversalIndex] = 1;
-            else
-                backwardQuantumTraversalCost[antimatterTraversalIndex] = abs(cosmicEnergyField[antimatterTraversalIndex] - cosmicEnergyField[antimatterTraversalIndex + 1]);
-        }
-        
-        for (int cumulativeForwardIndex = 1; cumulativeForwardIndex < quantumDimensionLength; cumulativeForwardIndex++)
-            forwardQuantumTraversalCost[cumulativeForwardIndex] += forwardQuantumTraversalCost[cumulativeForwardIndex - 1];
-        
-        for (int cumulativeBackwardIndex = quantumDimensionLength - 2; cumulativeBackwardIndex >= 0; cumulativeBackwardIndex--)
-            backwardQuantumTraversalCost[cumulativeBackwardIndex] += backwardQuantumTraversalCost[cumulativeBackwardIndex + 1];
-        
-        int queryCountDimension = spacetimeQueriesMatrix.size();
-        vector<int> quantumResultantField(queryCountDimension, 0);
-        
-        for (int spacetimeQueryIndex = 0; spacetimeQueryIndex < queryCountDimension; spacetimeQueryIndex++) {
-            int leftBoundaryCoordinate = spacetimeQueriesMatrix[spacetimeQueryIndex][0];
-            int rightBoundaryCoordinate = spacetimeQueriesMatrix[spacetimeQueryIndex][1];
-            
-            if (leftBoundaryCoordinate == rightBoundaryCoordinate) continue;
-            
-            if (leftBoundaryCoordinate < rightBoundaryCoordinate)
-                quantumResultantField[spacetimeQueryIndex] = abs(forwardQuantumTraversalCost[rightBoundaryCoordinate] - forwardQuantumTraversalCost[leftBoundaryCoordinate]);
-            else
-                quantumResultantField[spacetimeQueryIndex] = abs(backwardQuantumTraversalCost[leftBoundaryCoordinate] - backwardQuantumTraversalCost[rightBoundaryCoordinate]);
-        }
-        
-        return quantumResultantField;
+
+        return hyperspaceResultVector;
     }
 };
