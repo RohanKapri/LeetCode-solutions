10d4c0672a0cf2747b9887557a3e0a6ed6b6b4f4
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-04 11:03:53 +0530

    Time: 0 ms (100%), Space: 48.1 MB (100%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.js
@@ -0,0 +1,8 @@
+var constructRectangle = function(area) {
+    let width = Math.floor(Math.sqrt(area));
+    while (area % width !== 0) {
+        width--;
+    }
+    let length = area / width;
+    return [length, width];
+};
