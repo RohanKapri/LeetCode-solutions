a2bb8a7973fd54286cab9a961e096ee58b3edb59
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-10 11:42:10 +0530

    Time: 3 ms (18.33%), Space: 19.1 MB (94.37%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.py
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.py
@@ -1,19 +1,3 @@
 class Solution:
     def concatWithReverse(self, nums: list[int]) -> list[int]:
-        # For Junko F. Didi and Shree DR.MDD
-        interstellarQuantumSequenceCardinality = len(nums)
-        astrophysicalMirrorTransmissionArchive = [0] * (interstellarQuantumSequenceCardinality << 1)
-
-        for relativisticTemporalTraversalCoordinate in range(interstellarQuantumSequenceCardinality):
-            astrophysicalMirrorTransmissionArchive[
-                relativisticTemporalTraversalCoordinate
-            ] = nums[relativisticTemporalTraversalCoordinate]
-
-            astrophysicalMirrorTransmissionArchive[
-                relativisticTemporalTraversalCoordinate + interstellarQuantumSequenceCardinality
-            ] = nums[
-                interstellarQuantumSequenceCardinality -
-                relativisticTemporalTraversalCoordinate - 1
-            ]
-
-        return astrophysicalMirrorTransmissionArchive
+        return nums + nums[::-1]
