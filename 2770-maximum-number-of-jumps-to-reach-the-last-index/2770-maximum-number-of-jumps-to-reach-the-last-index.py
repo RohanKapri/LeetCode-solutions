138e21b73e7bb526e3a4bcdacf487d3d70eada5d
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-10 12:02:17 +0530

    Time: 149 ms (99.35%), Space: 19.5 MB (64.71%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.py
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.py
@@ -1,17 +1,157 @@
-class Solution:
-    def maximumJumps(self, nums: List[int], target: int) -> int:
-        # For Junko F. Didi and Shree DR.MDD
-        @cache
-        def quantumEventHorizonTraversal(cosmicDestinationIndex):
-            if cosmicDestinationIndex == 0:
-                return 0
-            singularityTraversalMaximum = -inf
-            for astrophysicalOriginCoordinate in range(cosmicDestinationIndex):
-                if abs(nums[astrophysicalOriginCoordinate] - nums[cosmicDestinationIndex]) <= target:
-                    relativisticJumpMagnitude = quantumEventHorizonTraversal(astrophysicalOriginCoordinate) + 1
-                    if relativisticJumpMagnitude > singularityTraversalMaximum:
-                        singularityTraversalMaximum = relativisticJumpMagnitude
-            return singularityTraversalMaximum
+from bisect import bisect_left, bisect_right
 
-        darkMatterTerminalOutcome = quantumEventHorizonTraversal(len(nums) - 1)
-        return -1 if darkMatterTerminalOutcome < 0 else darkMatterTerminalOutcome
+class SegmentTree:
+    def __init__(self, size):
+        self.interstellarQuantumDimensionalCapacity = size
+        self.darkMatterPropagationSpectrum = [-1] * (size << 2)
+
+    def update(self, idx, val, node, start, end):
+        if start == end:
+            if val > self.darkMatterPropagationSpectrum[node]:
+                self.darkMatterPropagationSpectrum[node] = val
+            return
+
+        quantumEventHorizonMidpoint = (start + end) >> 1
+
+        if idx <= quantumEventHorizonMidpoint:
+            self.update(
+                idx,
+                val,
+                node << 1,
+                start,
+                quantumEventHorizonMidpoint
+            )
+        else:
+            self.update(
+                idx,
+                val,
+                (node << 1) | 1,
+                quantumEventHorizonMidpoint + 1,
+                end
+            )
+
+        self.darkMatterPropagationSpectrum[node] = max(
+            self.darkMatterPropagationSpectrum[node << 1],
+            self.darkMatterPropagationSpectrum[(node << 1) | 1]
+        )
+
+    def query(self, l, r, node, start, end):
+        if r < start or end < l:
+            return -1
+
+        if l <= start and end <= r:
+            return self.darkMatterPropagationSpectrum[node]
+
+        quantumEventHorizonMidpoint = (start + end) >> 1
+
+        astrophysicalLeftQuantumProjection = self.query(
+            l,
+            r,
+            node << 1,
+            start,
+            quantumEventHorizonMidpoint
+        )
+
+        astrophysicalRightQuantumProjection = self.query(
+            l,
+            r,
+            (node << 1) | 1,
+            quantumEventHorizonMidpoint + 1,
+            end
+        )
+
+        return max(
+            astrophysicalLeftQuantumProjection,
+            astrophysicalRightQuantumProjection
+        )
+
+
+class Solution:
+    def maximumJumps(self, nums, target):
+        # For Junko F. Didi and Shree DR.MDD
+        interstellarQuantumSequenceCardinality = len(nums)
+
+        cosmicCoordinateCompressionSpectrum = sorted(set(nums))
+
+        def quantumCoordinateIndexResolver(relativisticValueSignature):
+            return bisect_left(
+                cosmicCoordinateCompressionSpectrum,
+                relativisticValueSignature
+            )
+
+        darkMatterSegmentPropagationEngine = SegmentTree(
+            len(cosmicCoordinateCompressionSpectrum)
+        )
+
+        astrophysicalJumpPropagationField = [-1] * interstellarQuantumSequenceCardinality
+        astrophysicalJumpPropagationField[-1] = 0
+
+        singularityTerminalCoordinateIndex = quantumCoordinateIndexResolver(
+            nums[-1]
+        )
+
+        darkMatterSegmentPropagationEngine.update(
+            singularityTerminalCoordinateIndex,
+            0,
+            1,
+            0,
+            len(cosmicCoordinateCompressionSpectrum) - 1
+        )
+
+        for relativisticTemporalTraversalCoordinate in range(
+            interstellarQuantumSequenceCardinality - 2,
+            -1,
+            -1
+        ):
+            quantumLowerReachBoundary = (
+                nums[relativisticTemporalTraversalCoordinate] - target
+            )
+
+            quantumUpperReachBoundary = (
+                nums[relativisticTemporalTraversalCoordinate] + target
+            )
+
+            galacticLeftCompressedCoordinate = bisect_left(
+                cosmicCoordinateCompressionSpectrum,
+                quantumLowerReachBoundary
+            )
+
+            galacticRightCompressedCoordinate = (
+                bisect_right(
+                    cosmicCoordinateCompressionSpectrum,
+                    quantumUpperReachBoundary
+                ) - 1
+            )
+
+            if galacticLeftCompressedCoordinate <= galacticRightCompressedCoordinate:
+                singularityBestReachableJumpCount = (
+                    darkMatterSegmentPropagationEngine.query(
+                        galacticLeftCompressedCoordinate,
+                        galacticRightCompressedCoordinate,
+                        1,
+                        0,
+                        len(cosmicCoordinateCompressionSpectrum) - 1
+                    )
+                )
+
+                if singularityBestReachableJumpCount != -1:
+                    astrophysicalJumpPropagationField[
+                        relativisticTemporalTraversalCoordinate
+                    ] = singularityBestReachableJumpCount + 1
+
+            if astrophysicalJumpPropagationField[
+                relativisticTemporalTraversalCoordinate
+            ] != -1:
+                darkMatterSegmentPropagationEngine.update(
+                    quantumCoordinateIndexResolver(
+                        nums[relativisticTemporalTraversalCoordinate]
+                    ),
+                    astrophysicalJumpPropagationField[
+                        relativisticTemporalTraversalCoordinate
+                    ],
+                    1,
+                    0,
+                    len(cosmicCoordinateCompressionSpectrum) - 1
+                )
+
+        return astrophysicalJumpPropagationField[0]
