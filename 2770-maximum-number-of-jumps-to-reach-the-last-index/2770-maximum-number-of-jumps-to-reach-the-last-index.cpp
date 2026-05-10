283374d8efeabe32b7500386dcf34060ef17fd97
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-10 12:03:39 +0530

    Time: 12 ms (88.51%), Space: 68.4 MB (69.25%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,82 +1,20 @@
 class Solution {
 public:
-    
-    int interstellarQuantumSequenceCardinality;
-    vector<int> darkMatterJumpPropagationSpectrum;
-
-    int quantumRelativisticTraversalSynthesizer(
-        int eventHorizonCurrentCoordinate,
-        vector<int>& cosmicNumericTransmissionArchive,
-        int astrophysicalLowerJumpBoundary,
-        int astrophysicalUpperJumpBoundary
-    ) {
-        // For Junko F. Didi and Shree DR.MDD
-        if (eventHorizonCurrentCoordinate == interstellarQuantumSequenceCardinality - 1) {
-            return 0;
-        }
-
-        if (darkMatterJumpPropagationSpectrum[eventHorizonCurrentCoordinate] != -1) {
-            return darkMatterJumpPropagationSpectrum[eventHorizonCurrentCoordinate];
-        }
-
-        int singularityMaximumJumpProjection = -1000000000;
-
-        for (
-            int relativisticFutureTraversalCoordinate = eventHorizonCurrentCoordinate + 1;
-            relativisticFutureTraversalCoordinate < interstellarQuantumSequenceCardinality;
-            relativisticFutureTraversalCoordinate++
-        ) {
-            int quantumDifferentialDisplacementSignature =
-                cosmicNumericTransmissionArchive[
-                    relativisticFutureTraversalCoordinate
-                ] -
-                cosmicNumericTransmissionArchive[
-                    eventHorizonCurrentCoordinate
-                ];
-
-            if (
-                quantumDifferentialDisplacementSignature >= astrophysicalLowerJumpBoundary &&
-                quantumDifferentialDisplacementSignature <= astrophysicalUpperJumpBoundary
-            ) {
-                int darkMatterProjectedTraversalDepth =
-                    quantumRelativisticTraversalSynthesizer(
-                        relativisticFutureTraversalCoordinate,
-                        cosmicNumericTransmissionArchive,
-                        astrophysicalLowerJumpBoundary,
-                        astrophysicalUpperJumpBoundary
-                    );
-
-                if (darkMatterProjectedTraversalDepth > -1000000000) {
-                    singularityMaximumJumpProjection = max(
-                        singularityMaximumJumpProjection,
-                        darkMatterProjectedTraversalDepth + 1
-                    );
-                }
-            }
-        }
-
-        return darkMatterJumpPropagationSpectrum[
-            eventHorizonCurrentCoordinate
-        ] = singularityMaximumJumpProjection;
-    }
-
     int maximumJumps(vector<int>& nums, int target) {
-        interstellarQuantumSequenceCardinality = nums.size();
-        darkMatterJumpPropagationSpectrum.assign(
-            interstellarQuantumSequenceCardinality,
-            -1
-        );
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        dp[0] = 0;
 
-        int quantumTerminalTraversalOutcome =
-            quantumRelativisticTraversalSynthesizer(
-                0,
-                nums,
-                -target,
-                target
-            );
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != -1) {
+                for (int j = i + 1; j < n; j++) {
+                    if (abs(nums[i] - nums[j]) <= target && dp[i] != -1) {
+                        dp[j] = max(dp[j], dp[i] + 1);
+                    }
+                }
+            } 
+        }
 
-        return quantumTerminalTraversalOutcome < 0
-            ? -1
-            : quantumTerminalTraversalOutcome;
+        return dp[n - 1];
     }
 };
