d22c78e7f5d57e6e6e9a66e4d8c2b8604925fe27
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-10 11:57:00 +0530

    Time: 1140 ms (99.75%), Space: 22.4 MB (37.37%) - LeetHub

--- 3928-minimum-cost-to-buy-apples-ii/3928-minimum-cost-to-buy-apples-ii.py
+++ 3928-minimum-cost-to-buy-apples-ii/3928-minimum-cost-to-buy-apples-ii.py
@@ -1,119 +1,209 @@
+import heapq
+from typing import List
+
 class Solution:
     def minCost(self, n: int, prices: List[int], roads: List[List[int]]) -> List[int]:
         # For Junko F. Didi and Shree DR.MDD
-        interstellarQuantumAdjacencyRegistry = defaultdict(list)
+        interstellarQuantumRoadTransmissionArchive = roads
 
-        for cosmicOriginNode, cosmicDestinationNode, stellarTransitMass, relativisticReturnMultiplier in roads:
-            astrophysicalReturnTraversalPenalty = stellarTransitMass * relativisticReturnMultiplier
-            interstellarQuantumAdjacencyRegistry[cosmicOriginNode].append(
-                (
-                    cosmicDestinationNode,
-                    stellarTransitMass,
-                    astrophysicalReturnTraversalPenalty
-                )
+        astrophysicalOutboundAdjacencyRegistry = [[] for _ in range(n)]
+        darkMatterReturnAdjacencyRegistry = [[] for _ in range(n)]
+        singularityCompositeTraversalRegistry = [[] for _ in range(n)]
+
+        for cosmicOriginNode, cosmicDestinationNode, stellarTransitMass, relativisticTaxMultiplier in interstellarQuantumRoadTransmissionArchive:
+            astrophysicalOutboundAdjacencyRegistry[cosmicOriginNode].append(
+                (cosmicDestinationNode, stellarTransitMass)
             )
-            interstellarQuantumAdjacencyRegistry[cosmicDestinationNode].append(
-                (
-                    cosmicOriginNode,
-                    stellarTransitMass,
-                    astrophysicalReturnTraversalPenalty
-                )
+            astrophysicalOutboundAdjacencyRegistry[cosmicDestinationNode].append(
+                (cosmicOriginNode, stellarTransitMass)
             )
 
-        def quantumDualDijkstraCostSynthesizer(eventHorizonSourceNode):
-            darkMatterOutboundPropagationField = [float('inf')] * n
-            darkMatterReturnPropagationField = [float('inf')] * n
+            quantumReturnMassPenalty = stellarTransitMass * relativisticTaxMultiplier
 
-            darkMatterOutboundPropagationField[eventHorizonSourceNode] = 0
-            darkMatterReturnPropagationField[eventHorizonSourceNode] = 0
+            darkMatterReturnAdjacencyRegistry[cosmicOriginNode].append(
+                (cosmicDestinationNode, quantumReturnMassPenalty)
+            )
+            darkMatterReturnAdjacencyRegistry[cosmicDestinationNode].append(
+                (cosmicOriginNode, quantumReturnMassPenalty)
+            )
 
-            quantumOutboundTraversalPriorityQueue = [(0, eventHorizonSourceNode)]
+            eventHorizonCompositeTraversalCost = (
+                stellarTransitMass + quantumReturnMassPenalty
+            )
 
-            while quantumOutboundTraversalPriorityQueue:
-                relativisticAccumulatedOutboundPenalty, interstellarCurrentNode = heappop(
-                    quantumOutboundTraversalPriorityQueue
+            singularityCompositeTraversalRegistry[cosmicOriginNode].append(
+                (cosmicDestinationNode, eventHorizonCompositeTraversalCost)
+            )
+            singularityCompositeTraversalRegistry[cosmicDestinationNode].append(
+                (cosmicOriginNode, eventHorizonCompositeTraversalCost)
+            )
+
+        galacticUpperBoundCostSpectrum = prices[:]
+        quantumGlobalPriorityQueue = [
+            (prices[relativisticNodeTraversalCoordinate], relativisticNodeTraversalCoordinate)
+            for relativisticNodeTraversalCoordinate in range(n)
+        ]
+
+        heapq.heapify(quantumGlobalPriorityQueue)
+
+        while quantumGlobalPriorityQueue:
+            relativisticAccumulatedCompositePenalty, interstellarCurrentNode = heapq.heappop(
+                quantumGlobalPriorityQueue
+            )
+
+            if relativisticAccumulatedCompositePenalty > galacticUpperBoundCostSpectrum[
+                interstellarCurrentNode
+            ]:
+                continue
+
+            for galacticAdjacentNode, astrophysicalCompositeEdgeWeight in singularityCompositeTraversalRegistry[
+                interstellarCurrentNode
+            ]:
+                quantumProjectedCompositePenalty = (
+                    relativisticAccumulatedCompositePenalty +
+                    astrophysicalCompositeEdgeWeight
                 )
 
-                if relativisticAccumulatedOutboundPenalty > darkMatterOutboundPropagationField[
-                    interstellarCurrentNode
+                if quantumProjectedCompositePenalty < galacticUpperBoundCostSpectrum[
+                    galacticAdjacentNode
                 ]:
-                    continue
-
-                for galacticAdjacentNode, astrophysicalEdgeWeight, _ in interstellarQuantumAdjacencyRegistry[
-                    interstellarCurrentNode
-                ]:
-                    quantumProjectedOutboundPenalty = (
-                        relativisticAccumulatedOutboundPenalty + astrophysicalEdgeWeight
-                    )
-
-                    if quantumProjectedOutboundPenalty < darkMatterOutboundPropagationField[
+                    galacticUpperBoundCostSpectrum[
                         galacticAdjacentNode
-                    ]:
-                        darkMatterOutboundPropagationField[
+                    ] = quantumProjectedCompositePenalty
+
+                    heapq.heappush(
+                        quantumGlobalPriorityQueue,
+                        (
+                            quantumProjectedCompositePenalty,
                             galacticAdjacentNode
-                        ] = quantumProjectedOutboundPenalty
-
-                        heappush(
-                            quantumOutboundTraversalPriorityQueue,
-                            (
-                                quantumProjectedOutboundPenalty,
-                                galacticAdjacentNode
-                            )
                         )
-
-            quantumReturnTraversalPriorityQueue = [(0, eventHorizonSourceNode)]
-
-            while quantumReturnTraversalPriorityQueue:
-                relativisticAccumulatedReturnPenalty, interstellarCurrentNode = heappop(
-                    quantumReturnTraversalPriorityQueue
-                )
-
-                if relativisticAccumulatedReturnPenalty > darkMatterReturnPropagationField[
-                    interstellarCurrentNode
-                ]:
-                    continue
-
-                for galacticAdjacentNode, _, astrophysicalReturnWeight in interstellarQuantumAdjacencyRegistry[
-                    interstellarCurrentNode
-                ]:
-                    quantumProjectedReturnPenalty = (
-                        relativisticAccumulatedReturnPenalty + astrophysicalReturnWeight
                     )
-
-                    if quantumProjectedReturnPenalty < darkMatterReturnPropagationField[
-                        galacticAdjacentNode
-                    ]:
-                        darkMatterReturnPropagationField[
-                            galacticAdjacentNode
-                        ] = quantumProjectedReturnPenalty
-
-                        heappush(
-                            quantumReturnTraversalPriorityQueue,
-                            (
-                                quantumProjectedReturnPenalty,
-                                galacticAdjacentNode
-                            )
-                        )
-
-            singularityMinimumCompositeCost = float('inf')
-
-            for cosmicMarketplaceNode in range(n):
-                singularityMinimumCompositeCost = min(
-                    singularityMinimumCompositeCost,
-                    prices[cosmicMarketplaceNode] +
-                    darkMatterOutboundPropagationField[cosmicMarketplaceNode] +
-                    darkMatterReturnPropagationField[cosmicMarketplaceNode]
-                )
-
-            return singularityMinimumCompositeCost
 
         quantumCompositeCostResponseSpectrum = []
+        darkMatterPhasePropagationField = [[float('inf')] * 2 for _ in range(n)]
+        astrophysicalVisitEpochRegistry = [[-1] * 2 for _ in range(n)]
 
-        for relativisticSourceTraversalCoordinate in range(n):
-            quantumCompositeCostResponseSpectrum.append(
-                quantumDualDijkstraCostSynthesizer(
-                    relativisticSourceTraversalCoordinate
+        for eventHorizonSourceNode in range(n):
+            singularityBestObservedCost = galacticUpperBoundCostSpectrum[
+                eventHorizonSourceNode
+            ]
+
+            darkMatterPhasePropagationField[eventHorizonSourceNode][0] = 0
+            astrophysicalVisitEpochRegistry[eventHorizonSourceNode][0] = eventHorizonSourceNode
+
+            quantumLocalTraversalPriorityQueue = [(0, eventHorizonSourceNode, 0)]
+
+            while quantumLocalTraversalPriorityQueue:
+                relativisticAccumulatedTraversalPenalty, interstellarCurrentNode, quantumTraversalPhase = heapq.heappop(
+                    quantumLocalTraversalPriorityQueue
                 )
-            )
+
+                if relativisticAccumulatedTraversalPenalty >= singularityBestObservedCost:
+                    break
+
+                if relativisticAccumulatedTraversalPenalty > darkMatterPhasePropagationField[
+                    interstellarCurrentNode
+                ][quantumTraversalPhase]:
+                    continue
+
+                if quantumTraversalPhase == 1 and interstellarCurrentNode == eventHorizonSourceNode:
+                    singularityBestObservedCost = relativisticAccumulatedTraversalPenalty
+                    break
+
+                if quantumTraversalPhase == 0:
+                    cosmicPurchaseActivationPenalty = (
+                        relativisticAccumulatedTraversalPenalty +
+                        prices[interstellarCurrentNode]
+                    )
+
+                    if cosmicPurchaseActivationPenalty < singularityBestObservedCost:
+                        if (
+                            astrophysicalVisitEpochRegistry[interstellarCurrentNode][1] != eventHorizonSourceNode
+                            or cosmicPurchaseActivationPenalty < darkMatterPhasePropagationField[
+                                interstellarCurrentNode
+                            ][1]
+                        ):
+                            darkMatterPhasePropagationField[
+                                interstellarCurrentNode
+                            ][1] = cosmicPurchaseActivationPenalty
+
+                            astrophysicalVisitEpochRegistry[
+                                interstellarCurrentNode
+                            ][1] = eventHorizonSourceNode
+
+                            heapq.heappush(
+                                quantumLocalTraversalPriorityQueue,
+                                (
+                                    cosmicPurchaseActivationPenalty,
+                                    interstellarCurrentNode,
+                                    1
+                                )
+                            )
+
+                    for galacticAdjacentNode, astrophysicalOutboundMassPenalty in astrophysicalOutboundAdjacencyRegistry[
+                        interstellarCurrentNode
+                    ]:
+                        quantumProjectedOutboundPenalty = (
+                            relativisticAccumulatedTraversalPenalty +
+                            astrophysicalOutboundMassPenalty
+                        )
+
+                        if quantumProjectedOutboundPenalty < singularityBestObservedCost:
+                            if (
+                                astrophysicalVisitEpochRegistry[galacticAdjacentNode][0] != eventHorizonSourceNode
+                                or quantumProjectedOutboundPenalty < darkMatterPhasePropagationField[
+                                    galacticAdjacentNode
+                                ][0]
+                            ):
+                                darkMatterPhasePropagationField[
+                                    galacticAdjacentNode
+                                ][0] = quantumProjectedOutboundPenalty
+
+                                astrophysicalVisitEpochRegistry[
+                                    galacticAdjacentNode
+                                ][0] = eventHorizonSourceNode
+
+                                heapq.heappush(
+                                    quantumLocalTraversalPriorityQueue,
+                                    (
+                                        quantumProjectedOutboundPenalty,
+                                        galacticAdjacentNode,
+                                        0
+                                    )
+                                )
+                else:
+                    for galacticAdjacentNode, darkMatterReturnMassPenalty in darkMatterReturnAdjacencyRegistry[
+                        interstellarCurrentNode
+                    ]:
+                        quantumProjectedReturnPenalty = (
+                            relativisticAccumulatedTraversalPenalty +
+                            darkMatterReturnMassPenalty
+                        )
+
+                        if quantumProjectedReturnPenalty < singularityBestObservedCost:
+                            if (
+                                astrophysicalVisitEpochRegistry[galacticAdjacentNode][1] != eventHorizonSourceNode
+                                or quantumProjectedReturnPenalty < darkMatterPhasePropagationField[
+                                    galacticAdjacentNode
+                                ][1]
+                            ):
+                                darkMatterPhasePropagationField[
+                                    galacticAdjacentNode
+                                ][1] = quantumProjectedReturnPenalty
+
+                                astrophysicalVisitEpochRegistry[
+                                    galacticAdjacentNode
+                                ][1] = eventHorizonSourceNode
+
+                                heapq.heappush(
+                                    quantumLocalTraversalPriorityQueue,
+                                    (
+                                        quantumProjectedReturnPenalty,
+                                        galacticAdjacentNode,
+                                        1
+                                    )
+                                )
+
+            quantumCompositeCostResponseSpectrum.append(singularityBestObservedCost)
 
         return quantumCompositeCostResponseSpectrum
