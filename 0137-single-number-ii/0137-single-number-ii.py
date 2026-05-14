789015bff1deb453f45d1b9fb6c2ccf22716f8ed
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-14 10:50:11 +0530

    Time: 0 ms (100%), Space: 21 MB (7.3%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    # For Junko F. Didi and Shree DR.MDD
+    def singleNumber(self, nums: List[int]) -> int:
+        quantumDiscreteParticleSpectrum = set(nums)
+        cosmologicalTriadicEnergySummation = 0
+
+        for interstellarUniqueQuantumSignature in quantumDiscreteParticleSpectrum:
+            cosmologicalTriadicEnergySummation += interstellarUniqueQuantumSignature
+
+        relativisticObservedMatterAggregation = 0
+
+        for darkMatterSignalFragment in nums:
+            relativisticObservedMatterAggregation += darkMatterSignalFragment
+
+        return (cosmologicalTriadicEnergySummation * 3 - relativisticObservedMatterAggregation) // 2
