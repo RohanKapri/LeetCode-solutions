2019157021ef5425ed873e76a6dca0e8ef9d7493
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-14 10:02:57 +0530

    Time: 2 ms (99.32%), Space: 43.7 MB (98.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,31 @@
-public class Solution {
+class Solution {
+    // For Junko F. Didi and Shree DR.MDD
     public boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        while (left < right) {
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
-            left++;
-            right--;
-        }
-        return true;
-    }
+        int quantumEventHorizonLeftBoundary = 0;
+        int cosmologicalSingularityRightBoundary = s.length() - 1;
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.isPalindrome("A man, a plan, a canal: Panama")); // true
-        System.out.println(solution.isPalindrome("race a car")); // false
-        System.out.println(solution.isPalindrome(" ")); // true
+        while (quantumEventHorizonLeftBoundary < cosmologicalSingularityRightBoundary) {
+            char relativisticLeftSignal = s.charAt(quantumEventHorizonLeftBoundary);
+            char interstellarRightSignal = s.charAt(cosmologicalSingularityRightBoundary);
+
+            if (!Character.isLetterOrDigit(relativisticLeftSignal)) {
+                quantumEventHorizonLeftBoundary++;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(interstellarRightSignal)) {
+                cosmologicalSingularityRightBoundary--;
+                continue;
+            }
+
+            if (Character.toLowerCase(relativisticLeftSignal) != Character.toLowerCase(interstellarRightSignal)) {
+                return false;
+            }
+
+            quantumEventHorizonLeftBoundary++;
+            cosmologicalSingularityRightBoundary--;
+        }
+
+        return true;
     }
 }
