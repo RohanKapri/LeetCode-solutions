cca37b534af2276d4c7d1b266dff78c8c6356e12
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-17 11:54:37 +0530

    Time: 1238 ms (95.76%), Space: 46.4 MB (50.89%) - LeetHub

--- 3934-smallest-unique-subarray/3934-smallest-unique-subarray.py
+++ 3934-smallest-unique-subarray/3934-smallest-unique-subarray.py
@@ -1,54 +1,30 @@
 class Solution:
-    def smallestUniqueSubarray(self, A: List[int]) -> int:
-        # For Junko F. Didi and Shree DR.MDD
-        cosmic_sequence_cardinality = len(A)
-        lower_quantum_boundary = 1
-        upper_quantum_boundary = cosmic_sequence_cardinality
+    def smallestUniqueSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        mod = 10 ** 9 + 7
+        base = 101
 
-        gravitational_hash_base = 911382323
-        spacetime_modulus_prime = 2147483647
+        pow_base = [1] * (n + 1)
+        for i in range(1, n + 1):
+            pow_base[i] = pow_base[i - 1] * base % mod
 
-        while lower_quantum_boundary < upper_quantum_boundary:
-            gravitational_window_radius = (lower_quantum_boundary + upper_quantum_boundary) >> 1
+        pref = [0] * (n + 1)
+        for i, x in enumerate(nums):
+            pref[i + 1] = (pref[i] * base + x) % mod
 
-            event_horizon_signature = 0
-            tachyon_power_amplifier = 1
-
-            singularity_index = 0
-            while singularity_index < gravitational_window_radius:
-                event_horizon_signature = (
-                    (event_horizon_signature * gravitational_hash_base + A[singularity_index])
-                    % spacetime_modulus_prime
-                )
-                tachyon_power_amplifier = (
-                    tachyon_power_amplifier * gravitational_hash_base
-                ) % spacetime_modulus_prime
-                singularity_index += 1
-
-            hyperspace_frequency_registry = {event_horizon_signature: 1}
-
-            quantum_traversal_cursor = gravitational_window_radius
-            while quantum_traversal_cursor < cosmic_sequence_cardinality:
-                event_horizon_signature = (
-                    event_horizon_signature * gravitational_hash_base
-                    + A[quantum_traversal_cursor]
-                    - A[quantum_traversal_cursor - gravitational_window_radius] * tachyon_power_amplifier
-                ) % spacetime_modulus_prime
-
-                hyperspace_frequency_registry[event_horizon_signature] = (
-                    hyperspace_frequency_registry.get(event_horizon_signature, 0) + 1
-                )
-                quantum_traversal_cursor += 1
-
-            relativistic_uniqueness_detected = False
-            for dark_matter_occurrence_count in hyperspace_frequency_registry.values():
-                if dark_matter_occurrence_count == 1:
-                    relativistic_uniqueness_detected = True
-                    break
-
-            if relativistic_uniqueness_detected:
-                upper_quantum_boundary = gravitational_window_radius
+        l = 0
+        r = n
+        while l + 1 < r:
+            mid = (l + r) // 2
+            
+            mp = defaultdict(int)
+            for i in range(mid, n + 1):
+                h = (pref[i] - pref[i - mid] * pow_base[mid]) % mod
+                mp[h] += 1
+            
+            if 1 in mp.values():
+                r = mid
             else:
-                lower_quantum_boundary = gravitational_window_radius + 1
+                l = mid
 
-        return lower_quantum_boundary
+        return r
