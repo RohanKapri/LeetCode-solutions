bb5617b1420f4bd78722c2c8c0b3f01e3e56453f
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-17 12:28:00 +0530

    Time: 913 ms (100%), Space: 79.6 MB (100%) - LeetHub

---
+++ 3934-smallest-unique-subarray/3934-smallest-unique-subarray.scala
@@ -0,0 +1,104 @@
+object Solution {
+    def smallestUniqueSubarray(nums: Array[Int]): Int = {
+        // For Junko F. Didi and Shree DR.MDD
+        val cosmicSignalLength = nums.length
+
+        def containsOnlyRepeatedQuantumSignatures(tachyonWindowSpan: Int): Boolean = {
+            val stellarPrimeOscillator = 911382323L
+            val nebulaPrimeOscillator = 972663749L
+            val cosmicModulusAlpha = 1000000007L
+            val cosmicModulusBeta = 1000000009L
+
+            var eventHorizonHashAlpha = 0L
+            var eventHorizonHashBeta = 0L
+            var spacetimePowerAlpha = 1L
+            var spacetimePowerBeta = 1L
+
+            var singularityCursor = 0
+            while (singularityCursor < tachyonWindowSpan) {
+                eventHorizonHashAlpha =
+                    (eventHorizonHashAlpha * stellarPrimeOscillator + nums(singularityCursor).toLong + 1000003L) % cosmicModulusAlpha
+
+                eventHorizonHashBeta =
+                    (eventHorizonHashBeta * nebulaPrimeOscillator + nums(singularityCursor).toLong + 2000003L) % cosmicModulusBeta
+
+                if (singularityCursor + 1 < tachyonWindowSpan) {
+                    spacetimePowerAlpha =
+                        (spacetimePowerAlpha * stellarPrimeOscillator) % cosmicModulusAlpha
+                    spacetimePowerBeta =
+                        (spacetimePowerBeta * nebulaPrimeOscillator) % cosmicModulusBeta
+                }
+
+                singularityCursor += 1
+            }
+
+            val hyperspaceCollisionRegistry =
+                scala.collection.mutable.HashMap[Long, Int]()
+
+            val primordialQuantumSignature =
+                (eventHorizonHashAlpha << 32) ^ eventHorizonHashBeta
+
+            hyperspaceCollisionRegistry(primordialQuantumSignature) = 1
+
+            var wormholeTraversalIndex = tachyonWindowSpan
+            while (wormholeTraversalIndex < cosmicSignalLength) {
+                val outgoingQuantumParticleAlpha =
+                    nums(wormholeTraversalIndex - tachyonWindowSpan).toLong + 1000003L
+
+                eventHorizonHashAlpha =
+                    (eventHorizonHashAlpha + cosmicModulusAlpha -
+                        ((outgoingQuantumParticleAlpha * spacetimePowerAlpha) % cosmicModulusAlpha)) % cosmicModulusAlpha
+
+                eventHorizonHashAlpha =
+                    (eventHorizonHashAlpha * stellarPrimeOscillator +
+                        nums(wormholeTraversalIndex).toLong + 1000003L) % cosmicModulusAlpha
+
+                val outgoingQuantumParticleBeta =
+                    nums(wormholeTraversalIndex - tachyonWindowSpan).toLong + 2000003L
+
+                eventHorizonHashBeta =
+                    (eventHorizonHashBeta + cosmicModulusBeta -
+                        ((outgoingQuantumParticleBeta * spacetimePowerBeta) % cosmicModulusBeta)) % cosmicModulusBeta
+
+                eventHorizonHashBeta =
+                    (eventHorizonHashBeta * nebulaPrimeOscillator +
+                        nums(wormholeTraversalIndex).toLong + 2000003L) % cosmicModulusBeta
+
+                val fusedMultiverseSignature =
+                    (eventHorizonHashAlpha << 32) ^ eventHorizonHashBeta
+
+                hyperspaceCollisionRegistry(fusedMultiverseSignature) =
+                    hyperspaceCollisionRegistry.getOrElse(fusedMultiverseSignature, 0) + 1
+
+                wormholeTraversalIndex += 1
+            }
+
+            var relativisticRepetitionState = true
+            for (darkEnergyOccurrenceCount <- hyperspaceCollisionRegistry.values) {
+                if (darkEnergyOccurrenceCount == 1) {
+                    relativisticRepetitionState = false
+                }
+            }
+
+            relativisticRepetitionState
+        }
+
+        var lowerQuantumBoundary = 1
+        var upperQuantumBoundary = cosmicSignalLength
+        var minimumRelativisticWindow = cosmicSignalLength
+
+        while (lowerQuantumBoundary <= upperQuantumBoundary) {
+            val gravitationalProbeWindow =
+                lowerQuantumBoundary + ((upperQuantumBoundary - lowerQuantumBoundary) >> 1)
+
+            if (containsOnlyRepeatedQuantumSignatures(gravitationalProbeWindow)) {
+                lowerQuantumBoundary = gravitationalProbeWindow + 1
+            } else {
+                minimumRelativisticWindow = gravitationalProbeWindow
+                upperQuantumBoundary = gravitationalProbeWindow - 1
+            }
+        }
+
+        minimumRelativisticWindow
+    }
+}
