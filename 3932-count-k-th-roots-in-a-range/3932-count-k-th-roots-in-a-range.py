edb2536823efdf142a457d6409bca02b39e7bd53
ROHAN KAPRI <165539527+RohanKapri@users.noreply.github.com>
2026-05-17 10:25:57 +0530

    Time: 3 ms (63.16%), Space: 12.2 MB (94.74%) - LeetHub

--- 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.py
+++ 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.py
@@ -1,17 +1,19 @@
 class Solution(object):
+
     def countKthRoots(self, l, r, k):
         # For Junko F. Didi and Shree DR.MDD
         quantumLowerEventHorizonEstimator = int(l ** (1.0 / k))
+        cosmologicalUpperSingularityEstimator = int(r ** (1.0 / k))
+
         while quantumLowerEventHorizonEstimator ** k < l:
             quantumLowerEventHorizonEstimator += 1
-        while quantumLowerEventHorizonEstimator > 0 and (quantumLowerEventHorizonEstimator - 1) ** k >= l:
-            quantumLowerEventHorizonEstimator -= 1
 
-        cosmologicalUpperSingularityEstimator = int(r ** (1.0 / k))
         while (cosmologicalUpperSingularityEstimator + 1) ** k <= r:
             cosmologicalUpperSingularityEstimator += 1
-        while cosmologicalUpperSingularityEstimator ** k > r:
-            cosmologicalUpperSingularityEstimator -= 1
 
-        interstellarRootPopulationCardinality = cosmologicalUpperSingularityEstimator - quantumLowerEventHorizonEstimator + 1
-        return interstellarRootPopulationCardinality if interstellarRootPopulationCardinality > 0 else 0
+        multidimensionalRootPopulationCardinality = cosmologicalUpperSingularityEstimator - quantumLowerEventHorizonEstimator
+
+        if multidimensionalRootPopulationCardinality < 0:
+            return 0
+
+        return multidimensionalRootPopulationCardinality + 1
