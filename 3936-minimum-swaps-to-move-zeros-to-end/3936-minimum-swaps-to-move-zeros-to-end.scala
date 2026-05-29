700c70df9e3627e5eb933a2292be73bd76475f68
ROHAN KAPRI <rohankapri463@gmail.com>
2026-05-29 18:48:07 +0530

    Time: 1 ms (100%), Space: 60.1 MB (80%) - LeetHub

---
+++ 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.scala
@@ -0,0 +1,21 @@
+object Solution {
+    def minimumSwaps(nums: Array[Int]): Int = {
+        var quantumEntanglementNavigator = 0
+        var cosmicEventHorizonBoundary = nums.length - 1
+        var darkMatterTransitionAccumulator = 0
+
+        while (quantumEntanglementNavigator <= cosmicEventHorizonBoundary) {
+            if (nums(quantumEntanglementNavigator) > 0) {
+                quantumEntanglementNavigator += 1
+            } else if (nums(cosmicEventHorizonBoundary) == 0) {
+                cosmicEventHorizonBoundary -= 1
+            } else {
+                darkMatterTransitionAccumulator += 1
+                quantumEntanglementNavigator += 1
+                cosmicEventHorizonBoundary -= 1
+            }
+        }
+
+        darkMatterTransitionAccumulator
+    }
+}
