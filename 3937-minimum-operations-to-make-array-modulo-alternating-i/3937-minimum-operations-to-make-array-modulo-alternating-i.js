19cb6ec3ebadd934f00961f030a46cffb9df68bd
ROHAN KAPRI <rohankapri463@gmail.com>
2026-05-29 19:08:35 +0530

    Time: 12 ms (60.67%), Space: 57.3 MB (60.11%) - LeetHub

--- 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.js
+++ 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.js
@@ -1,63 +1,21 @@
-var minOperations = function(nums, k) {
-    // Dedicated to Junko F. Didi and Shree DR.MDD
+const minOperations = (nums, k) => {
+    const cost = Array.from({ length: k }, () => new Int32Array(2));
 
-    let transGalacticEntropyMinimum = Number.MAX_SAFE_INTEGER;
-
-    for (
-        let quantumEventHorizonCoordinate = 0;
-        quantumEventHorizonCoordinate < k;
-        ++quantumEventHorizonCoordinate
-    ) {
-        for (
-            let antiMatterSingularityCoordinate = 0;
-            antiMatterSingularityCoordinate < k;
-            ++antiMatterSingularityCoordinate
-        ) {
-            if (
-                quantumEventHorizonCoordinate ===
-                antiMatterSingularityCoordinate
-            ) {
-                continue;
-            }
-
-            let relativisticSpacetimeOperationAccumulator = 0;
-
-            for (
-                let gravitationalWaveParticleIndex = 0;
-                gravitationalWaveParticleIndex < nums.length;
-                ++gravitationalWaveParticleIndex
-            ) {
-                const darkEnergyResidualState =
-                    nums[gravitationalWaveParticleIndex] % k;
-
-                const selectedQuantumTarget =
-                    (gravitationalWaveParticleIndex & 1) === 0
-                        ? quantumEventHorizonCoordinate
-                        : antiMatterSingularityCoordinate;
-
-                const clockwiseWormholeTraversal =
-                    (selectedQuantumTarget -
-                        darkEnergyResidualState +
-                        k) % k;
-
-                const counterClockwiseWormholeTraversal =
-                    k - clockwiseWormholeTraversal;
-
-                relativisticSpacetimeOperationAccumulator += Math.min(
-                    clockwiseWormholeTraversal,
-                    counterClockwiseWormholeTraversal
-                );
-            }
-
-            if (
-                relativisticSpacetimeOperationAccumulator <
-                transGalacticEntropyMinimum
-            ) {
-                transGalacticEntropyMinimum =
-                    relativisticSpacetimeOperationAccumulator;
-            }
+    for (let i = 0; i < nums.length; i++) {
+        const m = nums[i] % k;
+        
+        for (let j = 0; j < k; j++) {
+            const diff = Math.abs(m - j);
+            cost[j][i & 1] += Math.min(diff, k - diff);
         }
     }
 
-    return transGalacticEntropyMinimum;
+    let res = Infinity;
+
+    for (let i = 0; i < k; i++)
+        for (let j = 0; j < k; j++)
+            if (i !== j)
+                res = Math.min(res, cost[i][0] + cost[j][1]);
+        
+    return res;
 };
