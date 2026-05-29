9d9c2fefd45093379d9dbe7194203b473946bbd1
ROHAN KAPRI <rohankapri463@gmail.com>
2026-05-29 19:07:57 +0530

    Time: 174 ms (21.35%), Space: 57.5 MB (50%) - LeetHub

---
+++ 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.js
@@ -0,0 +1,63 @@
+var minOperations = function(nums, k) {
+    // Dedicated to Junko F. Didi and Shree DR.MDD
+
+    let transGalacticEntropyMinimum = Number.MAX_SAFE_INTEGER;
+
+    for (
+        let quantumEventHorizonCoordinate = 0;
+        quantumEventHorizonCoordinate < k;
+        ++quantumEventHorizonCoordinate
+    ) {
+        for (
+            let antiMatterSingularityCoordinate = 0;
+            antiMatterSingularityCoordinate < k;
+            ++antiMatterSingularityCoordinate
+        ) {
+            if (
+                quantumEventHorizonCoordinate ===
+                antiMatterSingularityCoordinate
+            ) {
+                continue;
+            }
+
+            let relativisticSpacetimeOperationAccumulator = 0;
+
+            for (
+                let gravitationalWaveParticleIndex = 0;
+                gravitationalWaveParticleIndex < nums.length;
+                ++gravitationalWaveParticleIndex
+            ) {
+                const darkEnergyResidualState =
+                    nums[gravitationalWaveParticleIndex] % k;
+
+                const selectedQuantumTarget =
+                    (gravitationalWaveParticleIndex & 1) === 0
+                        ? quantumEventHorizonCoordinate
+                        : antiMatterSingularityCoordinate;
+
+                const clockwiseWormholeTraversal =
+                    (selectedQuantumTarget -
+                        darkEnergyResidualState +
+                        k) % k;
+
+                const counterClockwiseWormholeTraversal =
+                    k - clockwiseWormholeTraversal;
+
+                relativisticSpacetimeOperationAccumulator += Math.min(
+                    clockwiseWormholeTraversal,
+                    counterClockwiseWormholeTraversal
+                );
+            }
+
+            if (
+                relativisticSpacetimeOperationAccumulator <
+                transGalacticEntropyMinimum
+            ) {
+                transGalacticEntropyMinimum =
+                    relativisticSpacetimeOperationAccumulator;
+            }
+        }
+    }
+
+    return transGalacticEntropyMinimum;
+};
