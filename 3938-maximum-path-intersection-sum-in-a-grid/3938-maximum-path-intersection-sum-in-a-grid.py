7bdb34329c2207e22505596dcb640da23121d5a5
ROHAN KAPRI <rohankapri463@gmail.com>
2026-05-30 09:38:28 +0530

    Time: 593 ms (100%), Space: 40.8 MB (71.43%) - LeetHub

--- 3938-maximum-path-intersection-sum-in-a-grid/3938-maximum-path-intersection-sum-in-a-grid.py
+++ 3938-maximum-path-intersection-sum-in-a-grid/3938-maximum-path-intersection-sum-in-a-grid.py
@@ -1,56 +1,47 @@
-# Dedicated to Junko F. Didi and Shree DR.MDD
-class Solution:
+class Solution(object):
     def maxScore(self, grid):
-        cosmic_lattice_rows, cosmic_lattice_columns = len(grid), len(grid[0])
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        answer = float('-inf')
 
-        def quantum_singularity_resonance(dark_matter_flux_stream):
-            event_horizon_extremum = float('-inf')
+        for horizontalBand in grid:
+            accumulatedField = horizontalBand[0]
 
-            spacetime_continuum_state = dark_matter_flux_stream[0]
+            for stellarIndex in range(1, len(horizontalBand)):
+                quantumValue = horizontalBand[stellarIndex]
 
-            for graviton_wavefront_pointer in range(1, len(dark_matter_flux_stream)):
-                quantum_entanglement_energy = max(
-                    spacetime_continuum_state + dark_matter_flux_stream[graviton_wavefront_pointer],
-                    dark_matter_flux_stream[graviton_wavefront_pointer - 1] +
-                    dark_matter_flux_stream[graviton_wavefront_pointer]
+                if accumulatedField + quantumValue > answer:
+                    answer = accumulatedField + quantumValue
+
+                accumulatedField = (
+                    quantumValue
+                    if accumulatedField < 0
+                    else accumulatedField + quantumValue
                 )
 
-                if quantum_entanglement_energy > event_horizon_extremum:
-                    event_horizon_extremum = quantum_entanglement_energy
+        rowCount = len(grid)
+        columnCount = len(grid[0])
 
-                spacetime_continuum_state = max(
-                    spacetime_continuum_state + dark_matter_flux_stream[graviton_wavefront_pointer],
-                    dark_matter_flux_stream[graviton_wavefront_pointer]
+        for spacetimeColumn in range(columnCount):
+            accumulatedField = grid[0][spacetimeColumn]
+
+            for gravitonRow in range(1, rowCount):
+                quantumValue = grid[gravitonRow][spacetimeColumn]
+
+                if accumulatedField + quantumValue > answer:
+                    answer = accumulatedField + quantumValue
+
+                accumulatedField = (
+                    quantumValue
+                    if accumulatedField < 0
+                    else accumulatedField + quantumValue
                 )
 
-            return event_horizon_extremum
+        for nebulaRow in range(1, rowCount - 1):
+            for singularityColumn in range(1, columnCount - 1):
+                if grid[nebulaRow][singularityColumn] > answer:
+                    answer = grid[nebulaRow][singularityColumn]
 
-        multiverse_peak_amplitude = float('-inf')
-
-        for intergalactic_energy_band in grid:
-            if len(intergalactic_energy_band) > 1:
-                multiverse_peak_amplitude = max(
-                    multiverse_peak_amplitude,
-                    quantum_singularity_resonance(intergalactic_energy_band)
-                )
-
-        for tachyon_column_axis in range(cosmic_lattice_columns):
-            transdimensional_particle_stream = [
-                grid[stellar_coordinate_index][tachyon_column_axis]
-                for stellar_coordinate_index in range(cosmic_lattice_rows)
-            ]
-
-            if len(transdimensional_particle_stream) > 1:
-                multiverse_peak_amplitude = max(
-                    multiverse_peak_amplitude,
-                    quantum_singularity_resonance(transdimensional_particle_stream)
-                )
-
-        for relativistic_spacetime_row in range(1, cosmic_lattice_rows - 1):
-            for quantum_vacuum_column in range(1, cosmic_lattice_columns - 1):
-                multiverse_peak_amplitude = max(
-                    multiverse_peak_amplitude,
-                    grid[relativistic_spacetime_row][quantum_vacuum_column]
-                )
-
-        return multiverse_peak_amplitude
+        return answer
