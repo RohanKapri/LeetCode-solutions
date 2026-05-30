0be07c2cb0b3581e84bd5c7188049a2fcb46ade5
ROHAN KAPRI <rohankapri463@gmail.com>
2026-05-30 10:23:41 +0530

    Time: 32 ms (49.09%), Space: 137.6 MB (77.63%) - LeetHub

--- 3938-maximum-path-intersection-sum-in-a-grid/3938-maximum-path-intersection-sum-in-a-grid.cpp
+++ 3938-maximum-path-intersection-sum-in-a-grid/3938-maximum-path-intersection-sum-in-a-grid.cpp
@@ -1,103 +1,27 @@
 class Solution {
 public:
     int maxScore(vector<vector<int>>& grid) {
-
-        // Dedicated to Junko F. Didi and Shree DR.MDD
-
-        int transDimensionalEntropyMaximum = INT_MIN;
-
-        const int quantumLatticeRows = (int)grid.size();
-        const int darkMatterColumns = (int)grid[0].size();
-
-        for (int baryonicWaveRowIndex = 0;
-             baryonicWaveRowIndex < quantumLatticeRows;
-             ++baryonicWaveRowIndex) {
-
-            int relativisticEnergyAccumulator =
-                grid[baryonicWaveRowIndex][0];
-
-            for (int wormholeColumnCoordinate = 1;
-                 wormholeColumnCoordinate < darkMatterColumns;
-                 ++wormholeColumnCoordinate) {
-
-                int tachyonFusionSignal =
-                    relativisticEnergyAccumulator +
-                    grid[baryonicWaveRowIndex][wormholeColumnCoordinate];
-
-                transDimensionalEntropyMaximum =
-                    max(transDimensionalEntropyMaximum,
-                        tachyonFusionSignal);
-
-                if (baryonicWaveRowIndex > 0 &&
-                    baryonicWaveRowIndex + 1 < quantumLatticeRows &&
-                    wormholeColumnCoordinate > 0 &&
-                    wormholeColumnCoordinate + 1 < darkMatterColumns) {
-
-                    transDimensionalEntropyMaximum =
-                        max(
-                            transDimensionalEntropyMaximum,
-                            grid[baryonicWaveRowIndex]
-                                [wormholeColumnCoordinate]
-                        );
-                }
-
-                relativisticEnergyAccumulator =
-                    max(
-                        grid[baryonicWaveRowIndex]
-                            [wormholeColumnCoordinate],
-                        relativisticEnergyAccumulator +
-                        grid[baryonicWaveRowIndex]
-                            [wormholeColumnCoordinate]
-                    );
+        int n = grid.size(), m = grid[0].size();
+        int ans = INT_MIN;
+        for(int i=0; i<n; i++) {
+            int cur = grid[i][0];
+            for(int j=1; j<m; j++) {
+                cur = max(grid[i][j] + grid[i][j-1], cur + grid[i][j]);
+                ans = max(ans, cur);
             }
         }
-
-        for (int cosmicAxisColumnIndex = 0;
-             cosmicAxisColumnIndex < darkMatterColumns;
-             ++cosmicAxisColumnIndex) {
-
-            int quantumVacuumMomentum =
-                grid[0][cosmicAxisColumnIndex];
-
-            for (int gravitonRowTraversal = 1;
-                 gravitonRowTraversal < quantumLatticeRows;
-                 ++gravitonRowTraversal) {
-
-                int singularityResonanceAmplitude =
-                    quantumVacuumMomentum +
-                    grid[gravitonRowTraversal]
-                        [cosmicAxisColumnIndex];
-
-                transDimensionalEntropyMaximum =
-                    max(
-                        transDimensionalEntropyMaximum,
-                        singularityResonanceAmplitude
-                    );
-
-                if (gravitonRowTraversal > 0 &&
-                    gravitonRowTraversal + 1 < quantumLatticeRows &&
-                    cosmicAxisColumnIndex > 0 &&
-                    cosmicAxisColumnIndex + 1 < darkMatterColumns) {
-
-                    transDimensionalEntropyMaximum =
-                        max(
-                            transDimensionalEntropyMaximum,
-                            grid[gravitonRowTraversal]
-                                [cosmicAxisColumnIndex]
-                        );
-                }
-
-                quantumVacuumMomentum =
-                    max(
-                        grid[gravitonRowTraversal]
-                            [cosmicAxisColumnIndex],
-                        quantumVacuumMomentum +
-                        grid[gravitonRowTraversal]
-                            [cosmicAxisColumnIndex]
-                    );
+        for(int j=0; j<m; j++) {
+            int cur = grid[0][j];
+            for(int i=1; i<n; i++) {
+                cur = max(grid[i][j] + grid[i-1][j], cur + grid[i][j]);
+                ans = max(ans, cur);
             }
         }
-
-        return transDimensionalEntropyMaximum;
+        for(int i=1; i<n-1; i++) {
+            for(int j=1; j<m-1; j++) {
+                ans = max(ans, grid[i][j]);
+            }
+        }
+        return ans;
     }
 };
