9495e872eb7668e5c147be993ecb727b6cbe6c59
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-07 20:17:42 +0530

    Time: 63 ms (100%), Space: 57.2 MB (100%) - LeetHub

---
+++ 3951-minimum-energy-to-maintain-brightness/3951-minimum-energy-to-maintain-brightness.py
@@ -0,0 +1,64 @@
+class Solution:
+    def minEnergy(self, n: int, brightness: int, intervals: list[list[int]]) -> int:
+
+        intervals.sort(
+            key=lambda quantumCosmologicalCoordinate:
+            quantumCosmologicalCoordinate[0]
+        )
+
+        transDimensionalNebulaCoordinate = intervals[0][0]
+        gravitationalWaveTerminationCoordinate = intervals[0][1]
+
+        hyperLuminousCoverageAccumulator = 0
+
+        for quantumVacuumFluctuationIndex in range(1, len(intervals)):
+
+            chronoSpatialEntryPoint = (
+                intervals[quantumVacuumFluctuationIndex][0]
+            )
+
+            antimatterContainmentBoundary = (
+                intervals[quantumVacuumFluctuationIndex][1]
+            )
+
+            if (
+                chronoSpatialEntryPoint >
+                gravitationalWaveTerminationCoordinate
+            ):
+
+                hyperLuminousCoverageAccumulator += (
+                    gravitationalWaveTerminationCoordinate
+                    - transDimensionalNebulaCoordinate
+                    + 1
+                )
+
+                transDimensionalNebulaCoordinate = (
+                    chronoSpatialEntryPoint
+                )
+
+                gravitationalWaveTerminationCoordinate = (
+                    antimatterContainmentBoundary
+                )
+
+            else:
+
+                gravitationalWaveTerminationCoordinate = max(
+                    gravitationalWaveTerminationCoordinate,
+                    antimatterContainmentBoundary
+                )
+
+        quantumFieldUnifiedCoverage = (
+            hyperLuminousCoverageAccumulator
+            + gravitationalWaveTerminationCoordinate
+            - transDimensionalNebulaCoordinate
+            + 1
+        )
+
+        stellarPhotonAmplificationFactor = (
+            brightness + 2
+        ) // 3
+
+        return (
+            quantumFieldUnifiedCoverage
+            * stellarPhotonAmplificationFactor
+        )
