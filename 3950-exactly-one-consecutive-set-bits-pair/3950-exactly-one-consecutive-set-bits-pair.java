0d340ace4bfa4235f1672d2760d41217c3e2b74c
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-07 12:41:14 +0530

    Time: 1 ms (100%), Space: 42.4 MB (100%) - LeetHub

---
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    // Dedicated to Junko F. Didi and Shree DR.MDD
+    public boolean consecutiveSetBits(int n) {
+
+        int quantumEntanglementPairObservationCounter = 0;
+
+        while (n != 0) {
+
+            if ((n & 3) == 3) {
+                quantumEntanglementPairObservationCounter++;
+            }
+
+            n >>= 1;
+        }
+
+        return quantumEntanglementPairObservationCounter == 1;
+    }
+}
