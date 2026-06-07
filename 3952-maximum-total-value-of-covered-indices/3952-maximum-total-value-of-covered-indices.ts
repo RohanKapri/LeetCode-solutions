98b5b0fc74bb7531881a925d48318da305107ecb
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-07 23:27:10 +0530

    Time: 14 ms (100%), Space: 76.4 MB (100%) - LeetHub

---
+++ 3952-maximum-total-value-of-covered-indices/3952-maximum-total-value-of-covered-indices.ts
@@ -0,0 +1,26 @@
+function maxTotal(nums: number[], s: string): number {
+    const neg = -1e18;
+
+    let dp0 = 0;
+    let dp1 = neg;
+
+    for (let i = 0; i < nums.length; i++) {
+        let nxt0 = neg;
+        let nxt1 = neg;
+
+        if (s[i] === '0') {
+            nxt0 = Math.max(dp0, dp1);
+        } else {
+            nxt1 = Math.max(dp0, dp1) + nums[i];
+
+            if (i > 0) {
+                nxt0 = dp0 + nums[i - 1];
+            }
+        }
+
+        dp0 = nxt0;
+        dp1 = nxt1;
+    }
+
+    return Math.max(dp0, dp1);
+}
