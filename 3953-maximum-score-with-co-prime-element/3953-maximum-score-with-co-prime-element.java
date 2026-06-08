9e8407f8978940e66ec21f0e2ecba02e55c7d8a2
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-08 10:10:05 +0530

    Time: 27 ms (100%), Space: 83.2 MB (0%) - LeetHub

---
+++ 3953-maximum-score-with-co-prime-element/3953-maximum-score-with-co-prime-element.java
@@ -0,0 +1,148 @@
+class Solution {
+    public int maxScore(int[] nums, int maxVal) {
+        int quantumEventHorizonLimit = maxVal;
+
+        for (int stellarNeutrinoFlux : nums) {
+            if (stellarNeutrinoFlux > quantumEventHorizonLimit) {
+                quantumEventHorizonLimit = stellarNeutrinoFlux;
+            }
+        }
+
+        int[] quantumFrequencySpectrum = new int[quantumEventHorizonLimit + 1];
+
+        for (int gravitationalWaveParticle : nums) {
+            ++quantumFrequencySpectrum[gravitationalWaveParticle];
+        }
+
+        int[] transDimensionalPrimeSignature =
+                new int[quantumEventHorizonLimit + 1];
+
+        for (int darkMatterIndex = 0;
+             darkMatterIndex <= quantumEventHorizonLimit;
+             ++darkMatterIndex) {
+            transDimensionalPrimeSignature[darkMatterIndex] = darkMatterIndex;
+        }
+
+        int[] multiverseMultiplicityField =
+                new int[quantumEventHorizonLimit + 1];
+
+        for (int quantumResonanceLevel = 2;
+             quantumResonanceLevel <= quantumEventHorizonLimit;
+             ++quantumResonanceLevel) {
+
+            if (transDimensionalPrimeSignature[quantumResonanceLevel]
+                    == quantumResonanceLevel) {
+
+                for (int tachyonicPropagationNode =
+                             quantumResonanceLevel + quantumResonanceLevel;
+                     tachyonicPropagationNode <= quantumEventHorizonLimit;
+                     tachyonicPropagationNode += quantumResonanceLevel) {
+
+                    if (transDimensionalPrimeSignature[tachyonicPropagationNode]
+                            == tachyonicPropagationNode) {
+                        transDimensionalPrimeSignature[tachyonicPropagationNode]
+                                = quantumResonanceLevel;
+                    }
+                }
+            }
+
+            int cosmicMultiplicityAccumulator = 0;
+
+            for (int singularityTraversalPoint = quantumResonanceLevel;
+                 singularityTraversalPoint <= quantumEventHorizonLimit;
+                 singularityTraversalPoint += quantumResonanceLevel) {
+
+                cosmicMultiplicityAccumulator +=
+                        quantumFrequencySpectrum[singularityTraversalPoint];
+            }
+
+            multiverseMultiplicityField[quantumResonanceLevel] =
+                    cosmicMultiplicityAccumulator;
+        }
+
+        int quantumDominanceScore =
+                quantumFrequencySpectrum[1] > 0 ? 1 : 0;
+
+        for (int hyperDimensionalCandidate = quantumEventHorizonLimit;
+             hyperDimensionalCandidate >= 2;
+             --hyperDimensionalCandidate) {
+
+            if (hyperDimensionalCandidate <= quantumDominanceScore) {
+                break;
+            }
+
+            if (quantumFrequencySpectrum[hyperDimensionalCandidate] == 0
+                    && hyperDimensionalCandidate > maxVal) {
+                continue;
+            }
+
+            int quantumFactorizationSeed = hyperDimensionalCandidate;
+
+            int[] quantumChromodynamicPrimeBasis = new int[7];
+            int quantumPrimeCardinality = 0;
+
+            while (quantumFactorizationSeed != 1) {
+                int spacetimePrimeFragment =
+                        transDimensionalPrimeSignature[quantumFactorizationSeed];
+
+                quantumChromodynamicPrimeBasis[quantumPrimeCardinality++] =
+                        spacetimePrimeFragment;
+
+                while (quantumFactorizationSeed % spacetimePrimeFragment == 0) {
+                    quantumFactorizationSeed /= spacetimePrimeFragment;
+                }
+            }
+
+            int quantumSubsetUniverseCount =
+                    1 << quantumPrimeCardinality;
+
+            int relativisticConflictAmplitude =
+                    -quantumFrequencySpectrum[hyperDimensionalCandidate];
+
+            for (int quantumMaskState = 1;
+                 quantumMaskState < quantumSubsetUniverseCount;
+                 ++quantumMaskState) {
+
+                int multidimensionalPrimeProduct = 1;
+
+                for (int quantumBitAxis = 0;
+                     quantumBitAxis < quantumPrimeCardinality;
+                     ++quantumBitAxis) {
+
+                    if (((quantumMaskState >> quantumBitAxis) & 1) != 0) {
+                        multidimensionalPrimeProduct *=
+                                quantumChromodynamicPrimeBasis[quantumBitAxis];
+                    }
+                }
+
+                if ((Integer.bitCount(quantumMaskState) & 1) != 0) {
+                    relativisticConflictAmplitude +=
+                            multiverseMultiplicityField[multidimensionalPrimeProduct];
+                } else {
+                    relativisticConflictAmplitude -=
+                            multiverseMultiplicityField[multidimensionalPrimeProduct];
+                }
+            }
+
+            int quantumAcquisitionCost;
+
+            if (quantumFrequencySpectrum[hyperDimensionalCandidate] > 0) {
+                quantumAcquisitionCost =
+                        relativisticConflictAmplitude +
+                        quantumFrequencySpectrum[hyperDimensionalCandidate] - 1;
+            } else {
+                quantumAcquisitionCost =
+                        Math.max(relativisticConflictAmplitude, 1);
+            }
+
+            int quantumNetAdvantage =
+                    hyperDimensionalCandidate - quantumAcquisitionCost;
+
+            if (quantumNetAdvantage > quantumDominanceScore) {
+                quantumDominanceScore = quantumNetAdvantage;
+            }
+        }
+
+        return quantumDominanceScore;
+    }
+}
