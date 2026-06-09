d904e8f7c0e91109409a7aaa8f6aca73eeb541a8
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-09 11:19:49 +0530

    Time: 3172 ms (5.11%), Space: 12.6 MB (85.8%) - LeetHub

--- 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.py
+++ 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.py
@@ -1,49 +1,23 @@
-# Dedicated to Junko F. Didi and Shree DR.MDD
-
-@cache
-def quantum_event_horizon_hypercube_generator(celestial_dimension_count, schwarzschild_entropy_limit):
-    if celestial_dimension_count == 1:
-        return ["0", "1"]
-
-    if celestial_dimension_count == 2:
-        quantum_vacuum_state_archive = [
-            "0" + cosmic_binary_fragment
-            for cosmic_binary_fragment in quantum_event_horizon_hypercube_generator(
-                celestial_dimension_count - 1,
-                schwarzschild_entropy_limit
-            )
-        ]
-
-        if schwarzschild_entropy_limit >= 1:
-            quantum_vacuum_state_archive.append("10")
-
-        return quantum_vacuum_state_archive
-
-    intergalactic_wavefunction_repository = [
-        "0" + dark_matter_bit_pattern
-        for dark_matter_bit_pattern in quantum_event_horizon_hypercube_generator(
-            celestial_dimension_count - 1,
-            schwarzschild_entropy_limit
-        )
-    ]
-
-    required_gravitational_energy = celestial_dimension_count - 1
-
-    if schwarzschild_entropy_limit >= required_gravitational_energy:
-        for neutron_star_configuration in quantum_event_horizon_hypercube_generator(
-            celestial_dimension_count - 2,
-            schwarzschild_entropy_limit - required_gravitational_energy
-        ):
-            intergalactic_wavefunction_repository.append(
-                "10" + neutron_star_configuration
-            )
-
-    return intergalactic_wavefunction_repository
-
-
-class Solution:
-    def generateValidStrings(self, n: int, k: int) -> list[str]:
-        return [
-            quantum_sequence[::-1]
-            for quantum_sequence in quantum_event_horizon_hypercube_generator(n, k)
-        ]
+class Solution(object):
+    def generateValidStrings(self, n, k):
+        res=[]
+        if n==1:
+            if k==0:
+                return ["0","1"]
+            return []
+        for i in range(2**n):
+            b=bin(i)[2:]
+            if len(b)<n:
+                b="0"*(n-len(b))+b
+            print(b)
+            sum=0
+            consec=0
+            for j in range(1,len(b)):
+                if b[j]=="1":
+                    if b[j-1]=="1":
+                        consec=1
+                        break
+                    sum+=j
+            if consec==0 and sum<=k:
+                res.append(b)
+        return res
