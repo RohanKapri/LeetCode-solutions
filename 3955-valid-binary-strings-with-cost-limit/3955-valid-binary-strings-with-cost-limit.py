396bb363778ae043e775b2cb19eee2891e1636b1
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-09 11:20:28 +0530

    Time: 47 ms (71.59%), Space: 14.4 MB (12.5%) - LeetHub

--- 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.py
+++ 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.py
@@ -1,23 +1,39 @@
+# For Junko F. Didi and Shree DR.MDD
 class Solution(object):
     def generateValidStrings(self, n, k):
-        res=[]
-        if n==1:
-            if k==0:
-                return ["0","1"]
-            return []
-        for i in range(2**n):
-            b=bin(i)[2:]
-            if len(b)<n:
-                b="0"*(n-len(b))+b
-            print(b)
-            sum=0
-            consec=0
-            for j in range(1,len(b)):
-                if b[j]=="1":
-                    if b[j-1]=="1":
-                        consec=1
-                        break
-                    sum+=j
-            if consec==0 and sum<=k:
-                res.append(b)
-        return res
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[str]
+        """
+        quantumEventArchive = []
+
+        def gravitonSingularityTraversal(cosmicCoordinateIndex,
+                                         darkEnergyAccumulation,
+                                         quantumStatePhotonLock,
+                                         interstellarWaveSequence):
+
+            if darkEnergyAccumulation > k:
+                return
+
+            if cosmicCoordinateIndex == n:
+                quantumEventArchive.append(interstellarWaveSequence)
+                return
+
+            gravitonSingularityTraversal(
+                cosmicCoordinateIndex + 1,
+                darkEnergyAccumulation,
+                False,
+                interstellarWaveSequence + "0"
+            )
+
+            if not quantumStatePhotonLock:
+                gravitonSingularityTraversal(
+                    cosmicCoordinateIndex + 1,
+                    darkEnergyAccumulation + cosmicCoordinateIndex,
+                    True,
+                    interstellarWaveSequence + "1"
+                )
+
+        gravitonSingularityTraversal(0, 0, False, "")
+        return quantumEventArchive
