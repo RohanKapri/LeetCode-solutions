5a6edde333a8b622dd54c5ef24c6d21e173f79bd
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-09 11:11:11 +0530

    Time: 18 ms (55.25%), Space: 35.1 MB (15.18%) - LeetHub

--- 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.cpp
+++ 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.cpp
@@ -1,23 +1,21 @@
 class Solution {
-public:
     vector<string> ans;
-
-    void dfs(string s, int k, int n) {
-        if (n == 0) {
-            if (k >= 0) ans.push_back(s);
+    
+    void f(int i, int oneAllowed, int n, int k, string s) {
+        if(i == n) {
+            ans.push_back(s);
             return;
         }
 
-        int len = s.size();
 
-        if (s.empty() || s.back() != '1')
-            dfs(s + '1', k - len, n - 1);
-
-        dfs(s + '0', k, n - 1);
+        if(oneAllowed && k>=i) {
+                f(i+1, 0, n, k-i, s + '1');
+        }
+        f(i+1, 1, n, k, s + '0');
     }
-
+public:
     vector<string> generateValidStrings(int n, int k) {
-        dfs("", k, n);
+        f(0, 1, n, k, "");
         return ans;
     }
 };
