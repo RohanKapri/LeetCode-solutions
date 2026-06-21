e56f67216f2dd246c9ef6b331352872c0052e1ca
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-21 20:59:50 +0530

    Time: 0 ms (100%), Space: 8.6 MB (40.37%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.c
@@ -0,0 +1,13 @@
+bool checkGoodInteger(int n) {
+    int ds = 0;
+    int ss = 0;
+
+    while (n > 0) {
+        int curr = n % 10;
+        ds += curr;
+        ss += curr * curr;
+        n /= 10;
+    }
+
+    return ss - ds >= 50;
+}
