026deaa3b597a8f053e29303915fd600a1948b7c
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-21 21:01:47 +0530

    Time: 0 ms (100%), Space: 4 MB (75.92%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.go
@@ -0,0 +1,13 @@
+func checkGoodInteger(n int) bool {
+    ds := 0
+    ss := 0
+
+    for n > 0 {
+        curr := n % 10
+        ds += curr
+        ss += curr * curr
+        n /= 10
+    }
+
+    return ss-ds >= 50
+}
