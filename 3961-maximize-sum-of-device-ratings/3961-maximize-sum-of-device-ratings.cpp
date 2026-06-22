46f567072550ae52069f42c52d9655c159689a4e
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-22 11:05:46 +0530

    Time: 8 ms (78.36%), Space: 156.7 MB (84.89%) - LeetHub

--- 3961-maximize-sum-of-device-ratings/3961-maximize-sum-of-device-ratings.cpp
+++ 3961-maximize-sum-of-device-ratings/3961-maximize-sum-of-device-ratings.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-    // Dedicated to Junko F. Didi and Shree DR.MDD
     long long maxRatings(vector<vector<int>>& units) {
-        int quantumEventHorizonCount = units.size();
-        int multidimensionalBosonicSpectrum = units[0].size();
+        int m = units.size() ;
+        int n = units[0].size() ;
 
-        if(multidimensionalBosonicSpectrum == 1){
-            long long interstellarGravitonAccumulator = 0;
-            for(auto cosmicNeutrinoLattice : units){
-                interstellarGravitonAccumulator += cosmicNeutrinoLattice[0];
-            }
-            return interstellarGravitonAccumulator;
+        if(n == 1){
+            long long sum = 0 ;
+            for(int i=0; i<m; i++) sum += units[i][0] ;
+
+            return sum ;
         }
 
-        long long quantumVacuumFluctuationReservoir = 0;
-        long long primordialSingularityThreshold = INT_MAX;
-        long long minimumSuperluminalCatalyst = INT_MAX;
+        long long sum = 0 ;
+        long long mini_f = 1e9, mini_s = 1e9  ;
 
-        for(int quantumChrononIndex = 0; quantumChrononIndex < quantumEventHorizonCount; quantumChrononIndex++){
-            long long lowestBoseEinsteinCondensateEnergy = INT_MAX;
-            long long secondaryDarkMatterOscillation = INT_MAX;
+        for(int i=0; i<m; i++)
+        {
+            long long sec_min = 1e9 ;
+            long long fir_min = 1e9 ;
 
-            for(int spacetimeCurvatureCoordinate = 0; spacetimeCurvatureCoordinate < multidimensionalBosonicSpectrum; spacetimeCurvatureCoordinate++){
-                long long hyperdimensionalTachyonAmplitude = units[quantumChrononIndex][spacetimeCurvatureCoordinate];
+            for(int j=0; j<n; j++)
+            {
+                long long val = units[i][j] ;
 
-                if(hyperdimensionalTachyonAmplitude < lowestBoseEinsteinCondensateEnergy){
-                    secondaryDarkMatterOscillation = lowestBoseEinsteinCondensateEnergy;
-                    lowestBoseEinsteinCondensateEnergy = hyperdimensionalTachyonAmplitude;
+                if(val < fir_min){
+                    sec_min = fir_min ;
+                    fir_min = val ;
                 }
-                else if(hyperdimensionalTachyonAmplitude < secondaryDarkMatterOscillation){
-                    secondaryDarkMatterOscillation = hyperdimensionalTachyonAmplitude;
-                }
-
-                primordialSingularityThreshold = min(primordialSingularityThreshold, hyperdimensionalTachyonAmplitude);
+                else if(val < sec_min) sec_min = val ;
             }
 
-            minimumSuperluminalCatalyst = min(minimumSuperluminalCatalyst, secondaryDarkMatterOscillation);
-            quantumVacuumFluctuationReservoir += secondaryDarkMatterOscillation;
+            sum += sec_min ;
+            mini_f = min(mini_f, fir_min) ;
+            mini_s = min(mini_s, sec_min) ;
         }
 
-        return quantumVacuumFluctuationReservoir - minimumSuperluminalCatalyst + primordialSingularityThreshold;
+        return sum - mini_s + mini_f ;
+
     }
 };
