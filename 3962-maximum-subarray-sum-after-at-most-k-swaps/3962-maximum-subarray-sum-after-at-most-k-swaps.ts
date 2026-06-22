da5aac0a49229649c47f8da09ebc18d8b1d1954d
ROHAN KAPRI <rohankapri463@gmail.com>
2026-06-22 16:24:39 +0530

    Time: 869 ms (100%), Space: 82.3 MB (0%) - LeetHub

---
+++ 3962-maximum-subarray-sum-after-at-most-k-swaps/3962-maximum-subarray-sum-after-at-most-k-swaps.ts
@@ -0,0 +1,101 @@
+function maxSum(nums: number[], k: number): number {
+    const n = nums.length;
+
+    let ans = -2147483648;
+
+    if (k === 0 || n === 1) {
+        let sm = 0;
+        for (const num of nums) {
+            sm += num;
+            ans = Math.max(ans, sm);
+            if (sm < 0) sm = 0;
+        }
+        return ans;
+    }
+
+    let cnt = 0;
+    let posSum = 0;
+    const pref: number[] = new Array(n + 1).fill(0);
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] >= 0) posSum += nums[i];
+        else cnt++;
+
+        pref[i + 1] = pref[i] + nums[i];
+        ans = Math.max(ans, nums[i]);
+    }
+
+    if (cnt <= k) return posSum;
+
+    const dp: number[][] = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        const pq: number[] = [];
+        let sm = 0;
+
+        for (let j = i; j < n; j++) {
+            if (nums[j] >= 0) {
+                dp[i][j] = sm;
+                continue;
+            }
+
+            if (pq.length < k) {
+                pq.push(nums[j]);
+                pq.sort((a, b) => b - a);
+                sm += nums[j];
+            } else if (pq[0] > nums[j]) {
+                sm -= pq[0];
+                pq[0] = nums[j];
+                pq.sort((a, b) => b - a);
+                sm += nums[j];
+            }
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        const pqmax: number[] = [];
+        let sm = 0;
+
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < 0) continue;
+
+            if (pqmax.length < k) {
+                pqmax.push(nums[j]);
+                pqmax.sort((a, b) => a - b);
+                sm += nums[j];
+            } else if (pqmax[0] < nums[j]) {
+                sm -= pqmax[0];
+                pqmax[0] = nums[j];
+                pqmax.sort((a, b) => a - b);
+                sm += nums[j];
+            }
+        }
+
+        for (let j = n - 1; j > i; j--) {
+            let cur = pref[j + 1] - pref[i];
+            cur -= dp[i][j];
+            cur += sm;
+
+            ans = Math.max(ans, cur);
+
+            if (nums[j] < 0) continue;
+
+            if (pqmax.length < k) {
+                pqmax.push(nums[j]);
+                pqmax.sort((a, b) => a - b);
+                sm += nums[j];
+            } else if (pqmax[0] < nums[j]) {
+                sm -= pqmax[0];
+                pqmax[0] = nums[j];
+                pqmax.sort((a, b) => a - b);
+                sm += nums[j];
+            }
+
+            if (pqmax.length > 0) {
+                ans = Math.max(ans, sm);
+            }
+        }
+    }
+
+    return ans;
+}
