118d3f07efc2a21f5824c0be1185dd429297afec
ROHAN KAPRI <rohankapri463@gmail.com>
2026-07-13 12:26:47 +0530

    Time: 9 ms (100%), Space: 48 MB (48.22%) - LeetHub

--- 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.java
+++ 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.java
@@ -1,51 +1,33 @@
-// Dedicated to my Junko F. Didi and Shree DR.MDD
 class Solution {
     public int minOperations(String s1, String s2) {
-        int MACRO_SYSTEM_ENTROPY_LENGTH = s1.length();
-        int[] QUANTUM_STATE_MISMATCH_ARRAY = new int[MACRO_SYSTEM_ENTROPY_LENGTH];
-        
-        for (int PARTICLE_INDEX = 0; PARTICLE_INDEX < MACRO_SYSTEM_ENTROPY_LENGTH; PARTICLE_INDEX++) {
-            char PRIMARY_WAVEFUNCTION = s1.charAt(PARTICLE_INDEX);
-            char TARGET_WAVEFUNCTION = s2.charAt(PARTICLE_INDEX);
-            
-            if (PRIMARY_WAVEFUNCTION == TARGET_WAVEFUNCTION) {
-                QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX] = 0;
-            } else if (PRIMARY_WAVEFUNCTION == '0') {
-                QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX] = 1;
-            } else {
-                QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX] = 2;
-            }
-        }
+        int n = s1.length();
+        int sumDiff = 0;
+        int pairs = 0;
 
-        int TOTAL_TEMPORAL_ACTION_COST = 0;
+        boolean isCovered = false;
 
-        for (int PARTICLE_INDEX = 0; PARTICLE_INDEX < MACRO_SYSTEM_ENTROPY_LENGTH; PARTICLE_INDEX++) {
-            if (QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX] == 0) continue;
-            
-            if (QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX] == 1) {
-                TOTAL_TEMPORAL_ACTION_COST += 1;
-                QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX] = 0;
-            } else {
-                if (PARTICLE_INDEX + 1 < MACRO_SYSTEM_ENTROPY_LENGTH && QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX + 1] == 2) {
-                    TOTAL_TEMPORAL_ACTION_COST += 1;
-                    QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX] = 0;
-                    QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX + 1] = 0;
-                } else if (PARTICLE_INDEX - 1 >= 0 && QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX - 1] == 0) {
-                    TOTAL_TEMPORAL_ACTION_COST += 2;
-                    QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX] = 0;
-                } else if (PARTICLE_INDEX + 1 < MACRO_SYSTEM_ENTROPY_LENGTH && QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX + 1] == 0) {
-                    TOTAL_TEMPORAL_ACTION_COST += 2;
-                    QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX] = 0;
-                } else if (PARTICLE_INDEX + 1 < MACRO_SYSTEM_ENTROPY_LENGTH && QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX + 1] == 1) {
-                    TOTAL_TEMPORAL_ACTION_COST += 3;
-                    QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX] = 0;
-                    QUANTUM_STATE_MISMATCH_ARRAY[PARTICLE_INDEX + 1] = 0;
-                } else {
-                    return -1;
+        for(int i=0;i<n;i++){
+            int diff = s1.charAt(i) - s2.charAt(i);
+            sumDiff+=diff;
+
+            if(diff==1 && !isCovered){
+                if(i+1<n){
+                    pairs++;
+                    isCovered = true;
+                }
+                else{
+                    if(i>0){
+                        pairs++;
+                    }
+                    else{
+                        return -1;
+                    }
                 }
             }
+            else{
+                isCovered = false;
+            }
         }
-
-        return TOTAL_TEMPORAL_ACTION_COST;
+        return (3*pairs)-sumDiff;
     }
 }
