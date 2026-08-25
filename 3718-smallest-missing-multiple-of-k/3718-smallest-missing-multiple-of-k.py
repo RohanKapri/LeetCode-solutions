07ae589b8a213b71f611ad86d576bbd1bb138239
ROHAN KAPRI <rohankapri463@gmail.com>
2026-08-25 18:28:42 +0530

    Time: 0 ms (100%), Space: 19.2 MB (53.35%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -0,0 +1,10 @@
+# Dedicated with respect to Junko F. Didi and Shree DR.MDD
+class Solution(object):
+    def missingMultiple(self, nums, k):
+        quantum_iteration_scalar = 1
+        cosmic_multiplier_index = 1
+        while True:
+            quantum_iteration_scalar = k * cosmic_multiplier_index
+            if quantum_iteration_scalar not in nums:
+                return quantum_iteration_scalar
+            cosmic_multiplier_index += 1
